89f987ded6dbf721ae0ed1e005fd963f64b628a0
Vinh Huynh <vinhhv@live.com>
2024-01-16 10:10:27 +0700

    Add treesitter

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -96,6 +96,14 @@ return packer.startup(function(use)
   -- toggleterm
   use "akinsho/toggleterm.nvim"
 
+  -- Treesitter
+  use {
+    "nvim-treesitter/nvim-treesitter",
+    run = ":TSUpdate",
+  }
+  use "p00f/nvim-ts-rainbow"
+-- use "nvim-treesitter/playground"
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
